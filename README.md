9e34d55f8452576dd07b4fa2506a2feb1cd70286
Kellen Kittrell <kellenkittrell@gmail.com>
2022-10-03 20:19:29 -0700

    changed the function to use the result.concat instead of the math.random as i was having issues. Updated the readme file.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Password-Generator
+# Password-Generator-Homework
+
+## Description
+
+This password generator was very difficult for me. This was created as a project for my coding bootcamp. In its current state I have only been able to generate the string values as a whole but not split them randomly.
+
+
+## Installation
+
+No installation required. This project can be accessed from github pages or by going into the repo.
+
+## Usage
+
+This is still a work in progress and was intended to be able to generate random passwords based on user input to come up with a password on the screen the user could copy and use as a password if desired.
+
+
+
+## License
+N/A
--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"
-// var upperCase = ["A", "B", "C","D", "E", "F", "G", "H", "I"]
-var lengthPrompt = prompt("How long would you like your password to be?")
-var lowerCasePrompt = confirm("Would you like lower case letters?")
-var result = ""; 
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChar = "~!@#$%^&*()_+=<>,.?/";
+var passNumber = "1234567890";
+var lengthPrompt = prompt("How long would you like your password to be?");
+var upperCasePrompt = confirm("Would you like upper case letters?");
+var lowerCasePrompt = confirm("Would you like lower case letters?");
+var specialPrompt = confirm("Would you like special characters?");
+var numberPrompt = confirm("Would you like numbers?");
+var result = "";
+console.log(lowerCase + upperCase + specialChar + passNumber);
 
-function generatePassword () {
-  console.info("Generating Password...")
-  if (lowerCasePrompt ) {
-    console.log(lowerCasePrompt)
-    result = result + lowerCase[Math.round(Math.random() * Number.parseInt(lengthPrompt))] 
+function generatePassword() {
+  console.info("Generating Password...");
+  if (lowerCasePrompt) {
+    console.log(lowerCasePrompt);
+    result = result.concat(lowerCase, upperCase, specialChar, passNumber);
+    console.log(result);
+    // result = result + lowerCase[Math.round(Math.random() * Number.parseInt(lengthPrompt))]
+    // result = result + upperCase[Math.round(Math.random() * Number.parseInt(lengthPrompt))]
+    // result = result + specialChar[Math.round(Math.random() * Number.parseInt(lengthPrompt))]
+    // result = result + passNumber[Math.round(Math.random() * Number.parseInt(lengthPrompt))]
   }
-console.log(result)
-  return result
+
+  return result;
 }
 
 // Write password to the #password input
